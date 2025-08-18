e25b15f88fbf10102c507c26ec78cad967aa600e
Romain <rprieto@users.noreply.github.com>
2025-08-18 20:23:00 +0000

    chore: remove dev dependency on fs-extra

--- test/clean.js
+++ test/clean.js
@@ -1,7 +1,8 @@
 const path = require('node:path')
-const fs = require('fs-extra')
+const fs = require('node:fs')
 
 // Clean up test data before the tests run
 const actual = path.join(__dirname, '..', 'test-data', 'actual')
-fs.emptyDirSync(actual)
+fs.rmSync(actual, { recursive: true, force: true })
+fs.mkdirSync(actual)
 console.log('Cleanup test data: done')
