fdefc756c59b9516c8cf687a578bd281322d8916
Romain <rprieto@users.noreply.github.com>
2025-08-18 20:17:18 +0000

    test: fix cleanup of test data lost in Jest migration

--- test/clean.js
+++ test/clean.js
@@ -1,8 +1,7 @@
 const path = require('node:path')
 const fs = require('fs-extra')
 
-module.exports = async () => {
-  // Clean up test data before the tests run
-  const actual = path.join(__dirname, '..', 'test-data', 'actual')
-  await fs.emptyDir(actual)
-}
+// Clean up test data before the tests run
+const actual = path.join(__dirname, '..', 'test-data', 'actual')
+fs.emptyDirSync(actual)
+console.log('Cleanup test data: done')
